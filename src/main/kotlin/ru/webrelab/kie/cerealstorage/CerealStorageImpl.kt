8cdc0859a1b0cd6dd0d2f5ae676b0926baface82
e.petukhova <e.petukhova@yadro.com>
2025-01-27 11:38:17 +0300

    CerealStorageImplTest and CerealStorageImpl are implemented

--- src/main/kotlin/ru/webrelab/kie/cerealstorage/CerealStorageImpl.kt
+++ src/main/kotlin/ru/webrelab/kie/cerealstorage/CerealStorageImpl.kt
@@ -19,5 +19,67 @@ class CerealStorageImpl(
     }
 
     private val storage = mutableMapOf<Cereal, Float>()
+    override fun addCereal(cereal: Cereal, amount: Float): Float {
+        checkAmountIsNegative(amount)
+
+        val containerBalance = amount - getSpace(cereal)
+
+        if (storageCapacity < amount + storage.values.sum()) {
+            throw IllegalStateException("Хранилище не позволяет разместить ещё один контейнер для новой крупы")
+        }
+        if (containerBalance <= 0f) {
+            storage.put(cereal, getAmount(cereal) + amount)
+            return 0f
+        } else {
+            storage.put(cereal, containerCapacity)
+            return containerBalance
+        }
+
+
+    }
+
+    override fun getCereal(cereal: Cereal, amount: Float): Float {
+        checkAmountIsNegative(amount)
+        val amountInContainer = getAmount(cereal)
+
+        if (amountInContainer == 0f) {
+            return 0f
+        } else if (amountInContainer < amount) {
+            storage.put(cereal, 0f)
+            removeContainer(cereal)
+            return amountInContainer
+        } else {
+            storage.put(cereal, amountInContainer - amount)
+            return amount
+        }
+    }
+
+    override fun removeContainer(cereal: Cereal): Boolean {
+        if (storage.getOrDefault(cereal, 0f) > 0f) {
+            return false
+        } else {
+            storage.remove(cereal)
+            return true
+        }
+
+    }
+
+    override fun getAmount(cereal: Cereal): Float {
+        return storage.getOrDefault(cereal, 0f)
+    }
+
+    override fun getSpace(cereal: Cereal): Float {
+        val result = containerCapacity - getAmount(cereal)
+        return result
+    }
+
+    override fun toString(): String {
+        return "sum of cereal in a storage " + storage.values.sum()
+    }
+
+    private fun checkAmountIsNegative(amount: Float) {
+        if (amount < 0f) throw IllegalArgumentException("Передано некорректное количество крупы")
+    }
+
 
 }
